ff770c945c43a097b4c5814b7cdc810022f65b69
phochs <me.accounts+github@runbox.com>
2015-11-16 08:27:34 +0100

    Added a config file, moved some files over and created an outline for the program flow.

---
+++ server/usr/share/autoinstall/config.new.php
@@ -0,0 +1,2 @@
+<?php
+    $config['configPath'] = '/etc/autoinstall';
---
+++ server/usr/share/autoinstall/web/index.php
@@ -0,0 +1,25 @@
+<?php
+    /*
+     * Program Flow:
+     * - Get the configuration
+     * - Check the listen addresses against the client IP address
+     * - Check what response is needed
+     * - If the request is a software list for the client:
+     *  - Get the client in the main configuration
+     *  - Get the groups the client is a member of
+     *  - Parse all software references info a list with included and excluded software
+     *  - Get the details for each software reference
+     *  - Create a JSON string for the response
+     * - If the request is to get a script:
+     *  - Get the details for the specified software
+     *  - Get the location of the script
+     *  - Return the contents of the file
+     */
+    require('config.php');
+    require_once('lib/spyc/Spyc.php');
+    $response = array();
+    $response['version'] = '0.1dev';
+    $response['host'] = 'NYI';
+    
+    $globalSettings = Spyc::YAMLLoad($config['configPath'].'\config\settings.yaml');
+    var_dump($globalSettings);
